e84e89878fd979ccf96e3ca7e4903ba2181ccd5c
lsill <924867750@qq.com>
2024-02-05 15:38:35 +0800

    add test2

--- server/src/test/reactor/s00/test2.cpp
+++ server/src/test/reactor/s00/test2.cpp
@@ -1,7 +1,21 @@
 //
 // Created by lsill on 2024/2/4.
 //
+#include "EventLoop.h"
+#include "Thread.h"
+
+muduo::EventLoop* g_loop;
+
+void threadFunc() {
+    g_loop->loop();
+}
+
 int main()
 {
+    muduo::EventLoop loop;
+    g_loop = &loop;
+    muduo::Thread t(threadFunc);
+    t.start();
+    t.join();
     return 0;
 }
