982aab279cd0e8356cf4ac382fd0d5a6066d4d8b
lsill <924867750@qq.com>
2024-02-06 16:51:33 +0800

    Channel

--- server/src/muduo/net/Channel.cpp
+++ server/src/muduo/net/Channel.cpp
@@ -24,7 +24,9 @@ Channel::Channel(EventLoop* loop, int fd__)
           logHup_(true),
           tied_(false),
           eventHandling_(false),
-          addedToLoop_(false){}
+          addedToLoop_(false)
+{
+}
 
 Channel::~Channel()
 {
@@ -32,34 +34,107 @@ Channel::~Channel()
     assert(!addedToLoop_);
     if (loop_->isInLoopThread())
     {
-        //assert(!loop_->hasChannel(this));
+        assert(!loop_->hasChannel(this));
     }
 }
 
-void Channel::update() {
+void Channel::tie(const std::shared_ptr<void>& obj)
+{
+    tie_ = obj;
+    tied_ = true;
+}
+
+void Channel::update()
+{
+    addedToLoop_ = true;
     loop_->updateChannel(this);
 }
 
-void Channel::remove() {
-
+void Channel::remove()
+{
+    assert(isNoneEvent());
+    addedToLoop_ = false;
+    loop_->removeChannel(this);
 }
 
-void Channel::handleEvent(muduo::Timestamp receiveTime) {
-    if (revents_ & POLLNVAL) {
-        LOG_WARN << "Channel::handle_event() POLLNVAL";
+void Channel::handleEvent(Timestamp receiveTime)
+{
+    std::shared_ptr<void> guard;
+    if (tied_)
+    {
+        guard = tie_.lock();
+        if (guard)
+        {
+            handleEventWithGuard(receiveTime);
+        }
     }
-    if (revents_ & (POLLERR | POLLNVAL)) {
-        if (errorCallback_) errorCallback_();
+    else
+    {
+        handleEventWithGuard(receiveTime);
+    }
+}
+
+void Channel::handleEventWithGuard(Timestamp receiveTime)
+{
+    eventHandling_ = true;
+    LOG_TRACE << reventsToString();
+    if ((revents_ & POLLHUP) && !(revents_ & POLLIN))
+    {
+        if (logHup_)
+        {
+            LOG_WARN << "fd = " << fd_ << " Channel::handle_event() POLLHUP";
+        }
+        if (closeCallback_) closeCallback_();
     }
 
-#if __linux__
-    if (revents_ & (POLLIN | POLLPRI)) {  //| POLLRDHUB
-#elif __APPLE__
-    if (revents_ & (POLLIN | POLLPRI )) {
-#endif
-        if (readCallback_) readCallback_(Timestamp());  // TODO 这里需要传参
+    if (revents_ & POLLNVAL)
+    {
+        LOG_WARN << "fd = " << fd_ << " Channel::handle_event() POLLNVAL";
     }
-    if (revents_ & POLLOUT) {
+
+    if (revents_ & (POLLERR | POLLNVAL))
+    {
+        if (errorCallback_) errorCallback_();
+    }
+    if (revents_ & (POLLIN | POLLPRI | POLLRDHUP))
+    {
+        if (readCallback_) readCallback_(receiveTime);
+    }
+    if (revents_ & POLLOUT)
+    {
         if (writeCallback_) writeCallback_();
     }
+    eventHandling_ = false;
+}
+
+string Channel::reventsToString() const
+{
+    return eventsToString(fd_, revents_);
+}
+
+string Channel::eventsToString() const
+{
+    return eventsToString(fd_, events_);
+}
+
+string Channel::eventsToString(int fd, int ev)
+{
+    std::ostringstream oss;
+    oss << fd << ": ";
+    if (ev & POLLIN)
+        oss << "IN ";
+    if (ev & POLLPRI)
+        oss << "PRI ";
+    if (ev & POLLOUT)
+        oss << "OUT ";
+    if (ev & POLLHUP)
+        oss << "HUP ";
+    if (ev & POLLRDHUP)
+        oss << "RDHUP ";
+    if (ev & POLLERR)
+        oss << "ERR ";
+    if (ev & POLLNVAL)
+        oss << "NVAL ";
+
+    return oss.str();
 }
